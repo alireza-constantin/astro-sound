be91f6cb3af0072c169f4d4a1ed9ebeee6f6f1c5
alireza <alirezasoheili1224@yahoo.com>
2023-09-01 02:31:40 +0330

    scaffold the project with react tailwind shadcn

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    printWidth: 100,
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    useTabs: true,
+    plugins: ["prettier-plugin-astro"],
+    pluginSearchDirs: false,
+}
